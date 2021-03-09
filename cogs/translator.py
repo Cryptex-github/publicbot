65443610aa34bed4428f4e47fc3a547034dfc5d2
Ali-TM-original <76595455+Ali-TM-original@users.noreply.github.com>
2021-03-09 18:06:02 +0500

    First Cog added Translation commands
    These commands only work with googletrans version 3.1.0a0

--- cogs/translator.py
+++ cogs/translator.py
@@ -1,1 +1,37 @@
+# works with only gooletrans version 3.1.0a0
+from googletrans import Translator
+from discord.ext import commands
 
+class Translator(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        
+    @commands.command()
+    async def translate(self,ctx,lang,*,args):
+        try:
+            translator = Translator()
+            a = translator.translate(args, dest=lang)
+            #await ctx.send(a.text)
+            embed = discord.Embed(title='Translator', color=0xC01FE4)
+            embed.add_field(name=f"Translated: {args} to", value=f'{lang}', inline=False)
+            embed.add_field(name="Content: ", value=f'{a.text}', inline=False)
+            await ctx.send(embed=embed)
+        except:
+            await ctx.send("Brain.exe stopped working")
+
+            
+    @commands.command()
+    async def detect(self,ctx,*,message):
+        try:
+            translator = Translator()
+            x = translator.detect(message)
+            print(x.confidence)
+            embed = discord.Embed(title="Langugae Detector", color=0XC01EF4)
+            embed.add_field(name='***Langugae:***', value=x.lang, inline=False)
+            await ctx.send(embed=embed)    
+        except:
+            await ctx.send("Error 404 Brain not found!")
+            
+def setup(bot):
+    bot.add_cog(Music(bot))            
