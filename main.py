551f1b53677dd7cda7c795613fad9e220aab9e23
Daishiky <77997662+Daishiky@users.noreply.github.com>
2021-03-09 13:55:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # Hi, here you can modify this repo with your code!
-# How? Just fork this repo, add your files and you've done!
-# When you've finish, send in #opensrc-pull into the discord server, the link of your fork repo, and in the less time, a mod adds your code here! have fun!
+# How? Just request to be a collaborator for this code, in our discord server (#opensrc-req), and wait for a mod reply!
 # Discord Server = https://discord.gg/ZcErEwmVYu
 
 import discord
